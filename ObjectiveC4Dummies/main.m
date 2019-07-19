d64b0bffe49cd133a8999d570412784e373413b4
Carlos Encalada <carlos.encalada@crifa.com>
2019-07-19 16:12:09 -0500

    Initial Commit

---
+++ ObjectiveC4Dummies/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC4Dummies
+//
+//  Created by Carlos on 7/19/19.
+//  Copyright © 2019 Carlos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
